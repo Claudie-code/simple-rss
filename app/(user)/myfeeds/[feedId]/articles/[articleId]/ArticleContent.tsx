399bf4358fe72900c867cf7a6eb37b7723c8a8b0
Claudie Gueguen <gueguen_c@yahoo.fr>
2024-09-16 14:47:19 +0200

    test hightlight

--- app/(user)/myfeeds/[feedId]/articles/[articleId]/ArticleContent.tsx
+++ app/(user)/myfeeds/[feedId]/articles/[articleId]/ArticleContent.tsx
@@ -7,9 +7,9 @@ import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
 
 const ArticleContent = ({ content }: { content: string }) => {
-  useEffect(() => {
-    hljs.highlightAll();
-  }, []);
+  // useEffect(() => {
+  //   hljs.highlightAll();
+  // }, []);
   const text = parse(content);
   return <div className="text-lg md-post break-words">{text}</div>;
 };
